0fceeabe76d060d8336b3b5010d7e7673a1e3567
Ellyf Bernardo Antunes <ellyfbernardo12@gmail.com>
2024-05-01 00:36:35 -0300

    prototype created

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Moviecontainer from './Components/Moviecontainer/Moviecontainer'
+
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='app'>
+        <Moviecontainer/>
+      </div>
+    </>
+  )
+}
+
+export default App
